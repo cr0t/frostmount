92c049635e542e3c7d3f74e12692c397dc89f7ed
Sergey Kuznetsov <kuznecov.sg@gmail.com>
2023-07-18 01:16:56 +0200

    Finish FrostmountWeb.Lobby interactive form

--- lib/frostmount_web/live/lobby.ex
+++ lib/frostmount_web/live/lobby.ex
@@ -1,14 +1,34 @@
 defmodule FrostmountWeb.Lobby do
   use FrostmountWeb, :live_view
 
-  def render(assigns) do
-    ~H"""
-    <p class="mb-2 text-center">
-      You're about to join the group of <i>adventurous wanderers</i>. Reveal yourself, stranger!
-    </p>
-    """
-  end
+  alias Frostmount.Core.Hero
+  alias Frostmount.Core.Hero.Validator, as: HeroValidator
 
   def mount(_params, _session, socket),
-    do: {:ok, socket}
+    do: {:ok, assign_form(socket)}
+
+  def handle_event("validate", %{"hero" => hero_params}, socket),
+    do: {:noreply, validate_form(socket, hero_params)}
+
+  def handle_event("join", _params, %{assigns: %{form: form}} = socket),
+    do: {:noreply, redirect(socket, to: ~p"/battlefield?#{form.params}")}
+
+  defp assign_form(socket) do
+    form =
+      Hero.new()
+      |> HeroValidator.changeset(%{})
+      |> to_form()
+
+    assign(socket, form: form)
+  end
+
+  defp validate_form(socket, hero_params) do
+    form =
+      Hero.new()
+      |> HeroValidator.changeset(hero_params)
+      |> Map.put(:action, :validate)
+      |> to_form()
+
+    assign(socket, form: form)
+  end
 end
